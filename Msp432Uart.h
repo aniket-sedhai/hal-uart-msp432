3c955d4409e59fac355c2af07bc649333701fca7
Aniket Sedhai <75154810+aniket-sedhai@users.noreply.github.com>
2024-09-24 15:52:56 -0500

    skeleton

---
+++ Msp432Uart.h
@@ -0,0 +1,32 @@
+/**
+ * @file Msp432Uart.h
+ * @author Aniket Sedhai (aniketsedhai@gmail.com)
+ * @brief This file defines Uart for Msp432 family.
+ * @version 0.1
+ * @date 2024-09-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MSP432_UART_H
+#define MSP432_UART_H
+
+/* ====================================================================================================================
+ * INCLUDES
+ * ====================================================================================================================
+ */
+
+#include "HwAbstractionLayerUartInterface.h"
+
+/* ====================================================================================================================
+ * CLASS DECLARATION
+ * ====================================================================================================================
+ */
+
+class Msp432Uart: public HwAbstractionLayerUartInterface
+{
+
+};
+
+#endif /*MSP432_UART_H*/
